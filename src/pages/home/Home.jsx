dfdc37ca5f7c18dcc5759058f787c40e1c316bca
gastondelacruz <gastondelacruz@gmail.com>
2024-03-04 16:12:55 -0300

    test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,6 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
 export const Home = () => {
-  const a = 1;
   return (
     <>
       <Header />
